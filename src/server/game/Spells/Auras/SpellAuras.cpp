afe1921df70bb6067a80c77a0ebf5cb6e499a68e
Nevan <sss.nevan@gmail.com>
2011-10-31 10:48:48 +0100

    * Implement Spell Sanctified Wrath * Clean Up Talent Speed of Light

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1117,21 +1117,16 @@ void Aura::HandleAuraSpecificMods(AuraApplication const * aurApp, Unit * caster,
             case SPELLFAMILY_PALADIN: // Speed of Light (talent)
                 if (GetId() == 82327)
                 {
-                    if (target->HasAura(85495)) // r1
+                    if (AuraEffect * aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_PALADIN, 5062, EFFECT_1))
                     {
-                        target->CastSpell(target, 85497, true);
-                        target->SetSpeed(MOVE_RUN, 1.2f, true);
+                        int32 basepoints0 = aurEff->GetAmount();
+                        caster->CastCustomSpell(caster, 85497, &basepoints0, NULL, NULL, true);
                     }
-                    if (target->HasAura(85498)) // r2
-                    {
-                        target->CastSpell(target, 85497, true);
-                        target->SetSpeed(MOVE_RUN, 1.4f, true);
-                    }
-                    if (target->HasAura(85499)) // r3
-                    {
-                        target->CastSpell(target, 85497, true);
-                        target->SetSpeed(MOVE_RUN, 1.6f, true);
-                    }
+                }
+                if (GetId() == 31884) // Sanctified Wrath
+                {
+                    if(caster->GetAuraEffect(SPELL_AURA_ADD_FLAT_MODIFIER, SPELLFAMILY_PALADIN, 3029, EFFECT_1))
+                        caster->CastSpell(caster, 57318, true);
                 }
                 break;
             case SPELLFAMILY_DEATHKNIGHT:
