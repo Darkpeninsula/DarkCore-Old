048b4bf78c69f88056da0c434e18cb0f46f22536
Darkrabbit <scazzato88@gmail.com>
2011-11-02 23:25:47 +0100

    Cmake/Options: Added a New Option in Cmake config list:
    * WITH_CONF
    deselect this option to remove world and auth configuration files
    
    Author::Drethek

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -79,4 +79,10 @@ if( WIN32 )
   endif()
 endif( WIN32 )
 
+if( WITH_CONF )
+  message("* Install Conf-files     : Yes (default)")
+else()
+  message("* Install Conf-files     : No")
+endif()
+
 message("")
