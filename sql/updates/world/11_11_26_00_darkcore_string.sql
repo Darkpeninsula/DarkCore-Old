4b0ea07f9ca20fc91b836b63e3f7d836a51a5c0c
Darkrabbit <scazzato88@gmail.com>
2011-11-26 20:59:58 +0100

    DB/Spell&String: Add Missing String into Database for Manage Pet Result on Cast Spell Tame Beast.
    Fix  Spell Proc:
    * improved steady shot
    * master marksman
    * arcane concentration(clearcasting)
    * rude interruption
    * Bloodsurge
    * invocation
    
    Thanks to Enturion

---
+++ sql/updates/world/11_11_26_00_darkcore_string.sql
@@ -0,0 +1,3 @@
+-- darkcore_string
+DELETE FROM `darkcore_string` WHERE `entry` = '11002';
+INSERT INTO `darkcore_string` (`entry`, `content_default`) VALUES ('11002', 'You have too many pets already');
