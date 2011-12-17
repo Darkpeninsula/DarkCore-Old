fff5f9df53560ba1206591ed021f1c7a6d4e76fd
kmishima <gennaro.mari@libero.it>
2011-12-17 14:22:35 +0100

    Fix stats modifiers and disenchant loot for item 67129 'Signet of High Arcanist Savor'

---
+++ sql/updates/world/11_12_16_00_item_template.sql
@@ -0,0 +1,2 @@
+--Fix stats and disenchant loot for item Signet of High Arcanist Savor = 67129
+UPDATE item_template SET stat_type1=7, stat_value1=286, stat_type2=5, stat_value2=190, stat_type3=31, stat_value3=121, stat_type4=32, stat_value4=131, RequiredDisenchantSkill=475, DisenchantId=83 WHERE entry=67129;
