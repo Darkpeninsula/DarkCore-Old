0c59ced1a8a7a7e6e9fa397f0d17013c9e8f18c0
kmishima <gennaro.mari@libero.it>
2011-12-17 23:57:28 +0100

    Replaced old game object with new gameobject for quest Crystals of Power

---
+++ sql/updates/world/11_12_17_00_gameobject.sql
@@ -0,0 +1,5 @@
+--Replaced old game object with new gameobject for quest Crystals of Power=24720
+UPDATE gameobject SET id=164658 WHERE id=164778; /*Blue*/
+UPDATE gameobject SET id=164659 WHERE id=164779; /*Green*/
+UPDATE gameobject SET id=164660 WHERE id=164780; /*Red*/
+UPDATE gameobject SET id=164661 WHERE id=164781; /*Yellow*/
