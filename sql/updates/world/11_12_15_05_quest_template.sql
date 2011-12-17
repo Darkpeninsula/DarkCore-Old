0066e849615709fc54aebca5efed91ad2dee9cbc
kmishima <gennaro.mari@libero.it>
2011-12-17 14:21:17 +0100

    Update faction datas for A/H quest version of 'What Are These Things?'

---
+++ sql/updates/world/11_12_15_05_quest_template.sql
@@ -0,0 +1,3 @@
+-- Fix faction for quest What Are These Things?=9994 e What Are These Things?=9995
+UPDATE quest_template SET RequiredRaces=2098253 WHERE entry=9994;
+UPDATE quest_template SET RequiredRaces=946 WHERE entry=9995;
