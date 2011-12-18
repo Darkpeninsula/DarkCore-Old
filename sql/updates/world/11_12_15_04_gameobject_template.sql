0a2ddf80ee3ae1b68d575498730fecb85280e1cb
Defrag <Thema89@email.it>
2011-12-18 01:51:29 +0100

    Cleanup fix for quest 'I work for the Horde!'

--- sql/updates/world/11_12_15_04_gameobject_template.sql
+++ sql/updates/world/11_12_15_04_gameobject_template.sql
@@ -8,7 +8,6 @@ update gameobject_template
 set data0=57, data1=183394
 where entry=183395 or entry=183396 or entry=183397;
 
---Fix not lootable gameobject for quest "I work for the horde!=10086 old value was 101"
-update `gameobject_template`
-set `faction`=0
-where `entry`=182797 or `entry`=182799;
+--Fix not lootable gameobject for quest "I work for the horde!=10086"
+UPDATE gameobject_loot_template SET ChanceOrQuestChance=-100 WHERE item IN (67419, 67420);
+UPDATE gameobject_template SET flags=4, data0=1690 WHERE entry IN (182936, 182938);
